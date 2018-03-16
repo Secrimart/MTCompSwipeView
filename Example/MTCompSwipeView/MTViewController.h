661958b5fc4521d13b19d4fe6eb16f07a3f6a75b
Secrimart <secrimart@aliyun.com>
2018-03-16 09:17:42 +0800

    Initial commit

---
+++ Example/MTCompSwipeView/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  MTCompSwipeView
+//
+//  Created by rstx_reg@aliyun.com on 03/16/2018.
+//  Copyright (c) 2018 rstx_reg@aliyun.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTViewController : UIViewController
+
+@end
